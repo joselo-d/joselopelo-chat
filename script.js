3e060435e811fb83dfb817e475ffe8bbd3f28dad
José Luis Díaz Dungey <josediazdungey@gmail.com>
2026-09-20 10:29:05 -0300

    Add Twitch player integration with online/offline events

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ boton.addEventListener("click", function () {
     }
 
 });
+// --------------------
+// TWITCH
+// --------------------
+
+const player = new Twitch.Player("twitch-player", {
+    channel: "joselopelo",
+    width: "100%",
+    height: 450,
+    autoplay: false
+});
+
+const offline = document.getElementById("offline");
+const twitchPlayer = document.getElementById("twitch-player");
+
+player.addEventListener(Twitch.Player.ONLINE, function () {
+
+    twitchPlayer.style.display = "block";
+    offline.classList.add("oculto");
+
+});
+
+player.addEventListener(Twitch.Player.OFFLINE, function () {
+
+    twitchPlayer.style.display = "none";
+    offline.classList.remove("oculto");
+
+});
